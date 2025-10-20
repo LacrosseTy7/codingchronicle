c758e6f95879a7fed853a5dd54c72cb3cfe11a91
LacrosseTy7 <30twilson@go.dsdmail.net>
2025-10-20 01:23:08 +0000

    add something to store the videos

---
+++ data/alskdjfhqpwoeiruty.js
@@ -0,0 +1,15 @@
+let videos = [
+    {
+        name: "Mr. Darkside",
+        camelCase: "mrDarkside",
+        duration: "3:44"
+    }, {
+        name: "Lego Vehicles Climb Walls",
+        camelCase: "legoClimbWalls",
+        duration: "9:00"
+    }, {
+        name: "Lego Vehicles Destroy Towers",
+        camelCase: "legoDestroyTowers",
+        duration: "15:47"
+    }
+]
