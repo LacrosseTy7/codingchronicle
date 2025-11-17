d9df5e85995fb0da43de5b6db77cfbd34cadd4a2
LacrosseTy7 <30twilson@go.dsdmail.net>
2025-11-17 14:40:04 -0700

    Delete scripts directory

--- scripts/header.js
+++
@@ -1,9 +0,0 @@
-function openOptions() {
-    document.querySelector('.right-section-header').innerHTML += `
-        <div class="options-page">
-            <button onclick="window.location.href = 'index.html'" class="home-page option">HOME</button>
-            <button onclick="window.location.href = 'quizzes.html'" class="quizzes-page option">Quizzes</button>
-        </div>
-    `
-    document.querySelector('.options-page').style.bottom = `-${screen.height}px`
-}
