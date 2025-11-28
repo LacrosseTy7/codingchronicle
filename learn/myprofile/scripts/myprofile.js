1380001d63b3b2a802fc13fc6ee91cec1410e907
LacrosseTy7 <30twilson@go.dsdmail.net>
2025-11-28 14:41:25 -0700

    Delete learn directory

--- learn/myprofile/scripts/myprofile.js
+++
@@ -1,6 +0,0 @@
-users.forEach((user) => {
-    if (user.name === currentUser) {
-        document.querySelector('.username-info').innerHTML += user.name
-        document.querySelector('.email-info').innerHTML += user.email
-    }
-})
